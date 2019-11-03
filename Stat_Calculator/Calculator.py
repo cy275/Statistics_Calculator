600ec5766419104588576d7fc0ddbcfd3fb04d0a
cy275 <cy275@njit.edu>
2019-11-03 13:22:12 -0500

    Fix: fixed some code for Z score calculation

---
+++ Stat_Calculator/Calculator.py
@@ -0,0 +1,13 @@
+from Stat_Calculator.Z_Score import z_score
+
+
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def z_score(self, z, x, μ, σ, n)
+        self.result = zscore(z, x, μ, σ, n)
+        return self.result
+
