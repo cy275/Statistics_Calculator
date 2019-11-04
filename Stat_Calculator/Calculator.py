f1790e4a8f5cefe73712c37abbb3ea56de9e7b90
cy275 <cy275@njit.edu>
2019-11-03 23:29:05 -0500

    Task: updated calculator file with mean, median, mode, and standard deviation calculation

--- Stat_Calculator/Calculator.py
+++ Stat_Calculator/Calculator.py
@@ -1,4 +1,8 @@
 from Stat_Calculator.Population_Mean import mean
+from Stat_Calculator.Median import median
+from Stat_Calculator.Mode import mode
+from Stat_Calculator.Standard_Deviation import standard_deviation
+
 from Stat_Calculator.Z_Score import z_score
 
 
@@ -8,8 +12,20 @@ class Calculator:
     def __init__(self):
         pass
 
-    def mean(self, a, b):
-        self.result = mean(a, b)
+    def mean(self, a):
+        self.result = mean(a)
+        return self.result
+
+    def median(self, a):
+        self.result = median(a)
+        return self.result
+
+    def mode(self,a):
+        self.result = mode(a)
+        return self.result
+
+    def std_dev(self, a, b):
+        self.result = standard_deviation(a, b)
         return self.result
 
     def z(self, z, x, μ, σ, n):
